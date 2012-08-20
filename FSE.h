12be3d52ef813303650ea26d6f38efe2a4524c50
Supreet Pal Singh <supreetpal@gmail.com>
2012-08-20 07:22:56 +0530

    Clean up and indentation

--- FSE.h
+++ FSE.h
@@ -21,59 +21,55 @@ FSE_init(struct FSE_ucode *FSE)
 static inline void
 FSE_fini(struct FSE_ucode *FSE)
 {
-	do {
-		*FSE->ptr.u08++ = 0x7f;
-		FSE->len = FSE->ptr.u08 - FSE->data;
-	} while (FSE->len & 3);
-	FSE->ptr.u08 = FSE->data;
+	 *FSE->ptr.u08 = 0xff; 
+	 FSE->ptr.u08 = FSE->data;
 }
 
-
+static inline void
 FSE_delay_ns(struct FSE_ucode *FSE, u64 delay_ns)
 {
-  if (delay_ns <= 0xffff * 32) {
-    *FSE->ptr.u08++ = 0x01;
-    *FSE->ptr.u16++ = delay_ns / 32;
-  } else if (delay_ns <= 0xffff * 1000) {
-    /* wait some micro seconds */
-    *FSE->ptr.u08++ = 0x2;
-    *FSE->ptr.u16++ = delay_ns / 1000;
+      if (delay_ns <= 0xffff * 32) {
+		*FSE->ptr.u08++ = 0x01;
+		*FSE->ptr.u16++ = delay_ns / 32;
+      } else if (delay_ns <= 0xffff * 1000) {
+		/* wait some micro seconds */
+		*FSE->ptr.u08++ = 0x2;
+		*FSE->ptr.u16++ = delay_ns / 1000;
  
-    /* complete the delay with ns */
-    if (delay_ns % 1000 >= 32) {
-      *FSE->ptr.u08++ = 0x1;
-      *FSE->ptr.u16++ = (delay_ns % 1000) / 32;
-    }
-  } else {
-    /* we could try harder to optimize the output, but that should
-     * be sufficient since most waits will be under 65ms anyway.
-     *
-     * If we need to wait longer quite often, then a new opcode
-     * could be added.
-     */
-    *FSE->ptr.u08++ = 0x0;
-    *FSE->ptr.u32++ = (delay_ns >> 32);
-    *FSE->ptr.u32++ = (delay_ns & 0xffffffff);
-  }
+	    /* complete the delay with ns */
+	    if (delay_ns % 1000 >= 32) {
+		*FSE->ptr.u08++ = 0x1;
+		*FSE->ptr.u16++ = (delay_ns % 1000) / 32;
+	    }
+      } else {
+	/* we could try harder to optimize the output, but that should
+	* be sufficient since most waits will be under 65ms anyway.
+	*
+	* If we need to wait longer quite often, then a new opcode
+	* could be added.
+	*/
+		*FSE->ptr.u08++ = 0x0;
+		*FSE->ptr.u32++ = (delay_ns >> 32);
+		*FSE->ptr.u32++ = (delay_ns & 0xffffffff);
+      }
 }
 
-
+static inline void
 FSE_write(struct FSE_ucode *FSE, u32 reg, u32 val)
 {
 	if (val & 0xff == val) {
 		*FSE->ptr.u08++ = 0x11;
 		*FSE->ptr.u08++ = val;
-		*FSE->ptr.u32++ = reg;
-		
+		*FSE->ptr.u32++ = reg;		
 	} else {
 		*FSE->ptr.u08++ = 0x10;
 		*FSE->ptr.u32++ = val;
-		*FSE->ptr.u32++ = reg;
-			
+		*FSE->ptr.u32++ = reg;			
 	}
 
 }
 
+static inline void
 FSE_mask(struct FSE_ucode *FSE, u32 reg, u32 mask, u32 data)
 {
 	*FSE->ptr.u08++ = 0x12;
@@ -82,8 +78,8 @@ FSE_mask(struct FSE_ucode *FSE, u32 reg, u32 mask, u32 data)
 	*FSE->ptr.u32++ = data;  
 }
 
-
-FSE_mask(struct FSE_ucode *FSE, u32 reg, u32 mask, u32 data)
+static inline void
+FSE_wait(struct FSE_ucode *FSE, u32 reg, u32 mask, u32 data)
 {
 	*FSE->ptr.u08++ = 0x13;
 	*FSE->ptr.u32++ = reg;
@@ -91,6 +87,7 @@ FSE_mask(struct FSE_ucode *FSE, u32 reg, u32 mask, u32 data)
 	*FSE->ptr.u32++ = data;  
 }
 
+static inline void
 FSE_send_msg(struct FSE_ucode *FSE, u16 size, u8 *msg)
 {
 	int i = 0;
